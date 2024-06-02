5c7ef0ed16f060d5d9ebc2983aa3a83f4ca69cee
geissybastoss <geissybastos@icloud.com>
2024-06-02 14:20:32 -0400

    Desafio de Fluxo

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,13 @@
+
+public class ParametrosInvalidosException extends Exception {
+	
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+
+}
